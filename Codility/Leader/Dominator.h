7f0c8429a03b23c637678b58f96db0a0913633d4
magancarz <grzmateusz6@gmail.com>
2024-03-02 10:05:37 +0100

    [EXERCISE] Codility Leader - Dominator

---
+++ Codility/Leader/Dominator.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+
+int solution(std::vector<int>& A)
+{
+    int stack_size = 0;
+    int value;
+    for (int i = 0; i < A.size(); ++i)
+    {
+        if (stack_size == 0)
+        {
+            value = A[i];
+            ++stack_size;
+        }
+        else
+        {
+            if (value != A[i])
+            {
+                --stack_size;
+            }
+            else
+            {
+                ++stack_size;
+            }
+        }
+    }
+
+    int candidate = -1;
+    if (stack_size > 0)
+    {
+        candidate = value;
+    }
+
+    int count = 0;
+    int index = -1;
+    for (int i = 0; i < A.size(); ++i)
+    {
+        if (A[i] == candidate)
+        {
+            ++count;
+            index = i;
+        }
+    }
+
+    if (count > A.size() / 2)
+    {
+        return index;
+    }
+
+    return -1;
+}
